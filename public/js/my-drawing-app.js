3277d99e46d95b6b08550c54675b2d15da75d40a
Caedi Seim <caediseim@berkeley.edu>
2022-07-15 23:16:18 -0700

    pen thickness and eraser finished

--- public/js/my-drawing-app.js
+++ public/js/my-drawing-app.js
@@ -1,11 +1,14 @@
 /* The code for our drawing application! 
 Feel free to delete any/all of it and replace with your own functionality. */
 
-
-
 var path;
-var currentColor = 'black'
-var currentWidth = 5
+var currentColor = 'black';
+var lastColor = "black";
+var currentWidth = 5;
+var erased = false;
+
+// var myCircle = new Path.Circle(new Point(100, 70), 50);
+// myCircle.fillColor = currentColor;
 
 function onResize(event) {
 	// Whenever the window is resized, recenter the path:
@@ -31,7 +34,62 @@ $('#thick-green').on('click', function (e) { //jquery button click code for our 
     currentColor = 'green';
     currentWidth = 15;
 })
-$('#eraser').on('click', function (e) { //jquery button click code for our eraser button.
+
+// Sizing
+$('#xs').on('click', function (e) { //jquery button click code for our "green paint" button.
+    if (erased === true) {
+        currentColor = lastColor;
+    } 
+    currentWidth = 5;
+    erased = false;
+})
+$('#sm').on('click', function (e) { //jquery button click code for our "green paint" button.
+    if (erased === true) {
+        currentColor = lastColor;
+    } 
+    currentWidth = 10;
+    erased = false;
+
+})
+$('#md').on('click', function (e) { //jquery button click code for our "green paint" button.
+     if (erased === true) {
+        currentColor = lastColor;
+    } 
+    currentWidth = 20;
+    erased = false;
+
+})
+$('#lg').on('click', function (e) { //jquery button click code for our "green paint" button.
+    if (erased === true) {
+        currentColor = lastColor;
+    } 
+    currentWidth = 50;
+    erased = false;
+
+})
+
+// Eraser Sizing
+$('#eraser-md').on('click', function (e) { //jquery button click code for our eraser button.
+    lastColor = currentColor;
     currentColor = 'white';
     currentWidth = 20;
+    erased = true;
+})
+$('#eraser-sm').on('click', function (e) { //jquery button click code for our eraser button.
+    lastColor = currentColor;
+    currentColor = 'white';
+    currentWidth = 10;
+    erased = true;
+})
+$('#eraser-lg').on('click', function (e) { //jquery button click code for our eraser button.
+    lastColor = currentColor;
+    currentColor = 'white';
+    currentWidth = 50;
+    erased = true;
+})
+$('#eraser-xs').on('click', function (e) { //jquery button click code for our eraser button.
+    lastColor = currentColor;
+    currentColor = 'white';
+    currentWidth = 5;
+    erased = true;
 })
